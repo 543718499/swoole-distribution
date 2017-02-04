c4c4bdc7554309f31f9fcdfe4f9c8abe3f3dd5b5
ZhangPeng <zpen@vip.deyi.com>
2017-02-04 10:36:02 +0800

    new files

---
+++ src/config/redis.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: tmtbe
+ * Date: 16-7-14
+ * Time: 下午1:58
+ */
+
+/**
+ * 选择数据库环境
+ */
+$config['redis']['active'] = 'test';
+/**
+ * 本地环境
+ */
+$config['redis']['test']['ip'] = '192.168.21.10';
+$config['redis']['test']['port'] = 6379;
+$config['redis']['test']['select'] = 1;
+$config['redis']['test']['password'] = '123456';
+$config['redis']['asyn_max_count'] = 10;
+
+/**
+ * 最终的返回，固定写这里
+ */
+return $config;
